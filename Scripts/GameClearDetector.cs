dcd80934d59caeef03165fc916a7fe9eded90c65
Dyna-Rise <dynarise.headquarters@gmail.com>
2024-12-13 15:44:52 +0900

    12/13 GameClearDetector Added

---
+++ Scripts/GameClearDetector.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameClearDetector : MonoBehaviour
+{
+    public Hole holeRed;
+    public Hole holeBlue;
+    public Hole holeGreen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnGUI()
+    {
+        //全部のHoleのisHoldingがtrueなら
+        if (holeRed.IsHolding() && holeBlue.IsHolding() && holeGreen.IsHolding())
+        {
+            //簡易的なテキストをカメラに表示する
+            GUI.Label(new Rect(50,50,100,30),"Game Clear!");
+        }
+    }
+}
