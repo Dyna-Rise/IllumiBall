f39ec1f7ea57463349677151298c9291f09c548d
Dyna-Rise <dynarise.headquarters@gmail.com>
2025-06-24 11:28:18 +0900

    Holeプログラムまで

---
+++ Assets/Scripts/Hole.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hole : MonoBehaviour
+{
+    //どのボールを吸い寄せるかをタグで指定
+    public string targetTag;
+
+    private void OnTriggerStay(Collider other)
+    {
+        //コライダーに触れているオブジェクトのRigidbodyを取得
+        Rigidbody r = other.gameObject.GetComponent<Rigidbody>();
+
+        //ボールの方向を計算
+        Vector3 direction = other.gameObject.transform.position - transform.position;
+
+        //方向を正規化して directionを1（-1）に書き換える
+        direction.Normalize();
+
+        //タグに応じてボールに力を加える
+        if(other.gameObject.tag == targetTag)
+        {
+            //中心地点でボールを止めるため速度を減速させる
+            r.velocity *= 0.9f;
+            r.AddForce(direction * -20.0f,ForceMode.Acceleration);
+        }
+        else
+        {
+            //HoleからみてBall（相手）の方向に弾く
+            r.AddForce(direction * 80.0f,ForceMode.Acceleration);
+        }
+    }
+}
