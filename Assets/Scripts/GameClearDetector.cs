5ea1186b84e7299591e59c255774ceb477cb9f60
Dyna-Rise <dynarise.headquarters@gmail.com>
2025-06-24 13:57:53 +0900

    UGUI改造完了

--- Assets/Scripts/GameClearDetector.cs
+++ Assets/Scripts/GameClearDetector.cs
@@ -8,14 +8,35 @@ public class GameClearDetector : MonoBehaviour
     public Hole holeBlue;
     public Hole holeGreen;
 
-    //簡易ＵＩ
-    private void OnGUI()
+    public GameObject panel; //UIパネル
+
+    private void Start()
+    {
+        //最初は非表示
+        panel.SetActive(false);
+    }
+
+    private void Update()
     {
         //すべてのボールが入ったらラベル表示
-        if(holeRed.IsHolding() && holeBlue.IsHolding() && holeGreen.IsHolding())
+        if (holeRed.IsHolding() && holeBlue.IsHolding() && holeGreen.IsHolding())
         {
-            //画面より左から50の位置，上から50の位置、幅100、高さ30のテキストエリアを作り、文字を表示
-            GUI.Label(new Rect(50, 50, 100, 30), "Game Clear!");
+            panel.SetActive(true);
+        }
+        else
+        {
+            panel.SetActive(false);
         }
     }
+
+    //簡易ＵＩ
+    //private void OnGUI()
+    //{
+    //    //すべてのボールが入ったらラベル表示
+    //    if(holeRed.IsHolding() && holeBlue.IsHolding() && holeGreen.IsHolding())
+    //    {
+    //        //画面より左から50の位置，上から50の位置、幅100、高さ30のテキストエリアを作り、文字を表示
+    //        GUI.Label(new Rect(50, 50, 100, 30), "Game Clear!");
+    //    }
+    //}
 }
